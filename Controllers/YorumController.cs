953ac2e8338b70c27355aded183aaf51b0d360d9
Mustafa ali kağan küçük <65673292+malikagan48@users.noreply.github.com>
2022-01-24 11:56:12 +0300

    Add files via upload

---
+++ Controllers/YorumController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebProjesi.Models.DataContext;
+using WebProjesi.Models.Model;
+
+namespace WebProjesi.Controllers
+{
+    public class YorumController : Controller
+    {
+        private WebDBContext db = new WebDBContext();
+        public ActionResult Index()
+        {
+            var yorum = db.Yorum.Include(y => y.Blog).OrderBy(x=>x.BlogId);
+            return View(yorum.ToList());
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Yorum yorum = db.Yorum.Find(id);
+            if (yorum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yorum);
+        }
+        public ActionResult Create()
+        {
+            ViewBag.BlogId = new SelectList(db.Blog, "BlogId", "Baslik");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "YorumId,AdSoyad,Eposta,Icerik,Onay,BlogId")] Yorum yorum)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Yorum.Add(yorum);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BlogId = new SelectList(db.Blog, "BlogId", "Baslik", yorum.BlogId);
+            return View(yorum);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Yorum yorum = db.Yorum.Find(id);
+            if (yorum == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BlogId = new SelectList(db.Blog, "BlogId", "Baslik", yorum.BlogId);
+            return View(yorum);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "YorumId,AdSoyad,Eposta,Icerik,Onay,BlogId")] Yorum yorum)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(yorum).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BlogId = new SelectList(db.Blog, "BlogId", "Baslik", yorum.BlogId);
+            return View(yorum);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Yorum yorum = db.Yorum.Find(id);
+            if (yorum == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yorum);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Yorum yorum = db.Yorum.Find(id);
+            db.Yorum.Remove(yorum);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
